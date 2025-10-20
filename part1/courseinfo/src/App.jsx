d6a90b29180fbdb4a0882f60617d6333749dd4a1
khar20 <136043524+khar20@users.noreply.github.com>
2025-10-20 00:56:14 -0500

    exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,28 +2,22 @@ const Header = ({course}) => {
   return <h1>{course}</h1>
 }
 
+const Part = (props) => {
+  return <p>{props.part} {props.exercises}</p>
+}
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
   )
 }
 
 const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </div>
-  )
+  return <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
 }
 
 const App = () => {
